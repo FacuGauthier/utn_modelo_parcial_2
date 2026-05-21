bcff08d16fe81abc7eb0c1b5b13ab2b214bca21b
FacuGauthier <facundosgauthier@gmail.com>
2026-05-21 17:23:12 -0300

    import OnDelete in Prestamo

--- src/main/java/com/gfacu/modeloParcial/models/Prestamo.java
+++ src/main/java/com/gfacu/modeloParcial/models/Prestamo.java
@@ -2,6 +2,8 @@ package com.gfacu.modeloParcial.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDate;
 
